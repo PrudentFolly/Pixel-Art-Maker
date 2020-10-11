39dc5948dbcb7293a6339e32d5d16d844c8b2599
Jacob Villegas <jacobxxvillegas@gmail.com>
2020-10-10 19:38:25 -0700

    Adds function that will create a grid

--- designs.js
+++ designs.js
@@ -4,3 +4,22 @@ const COLOR_INPUT = document.querySelector('input[type=color]');
 const SIZE_INPUT = document.querySelectorAll('input[type=number]');
 // When size is submitted by the user, call makeGrid()
 document.querySelector('form').addEventListener('submit', makeGrid);
+// This function dynamically creates a grid, and clears existing ones
+function makeGrid() {
+  // Create grid using size input for dimensions
+  for(let i = 0; i < Number(SIZE_INPUT[0].value); i++){
+    // Create table row
+    const ROW = document.createElement('tr');
+    // Fill table row with cells
+    for(let i = 0; i < Number(SIZE_INPUT[1].value); i++){
+      // Create blank cell
+      const CELL = document.createElement('td');
+      // Append cell to table row
+      ROW.appendChild(CELL);
+    }
+    // Append table row to table
+    document.querySelector('table').appendChild(ROW);
+  }
+  // Prevent default behavior on submit event (was causing issues)
+  event.preventDefault();
+}
