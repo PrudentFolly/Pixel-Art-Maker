0470e5ca9c921b057220c1753b02773dc3e75c7c
Jacob Villegas <jacobxxvillegas@gmail.com>
2020-10-10 19:28:08 -0700

    Adds submit event listener to form

--- designs.js
+++ designs.js
@@ -2,3 +2,5 @@
 const COLOR_INPUT = document.querySelector('input[type=color]');
 // Select size input
 const SIZE_INPUT = document.querySelectorAll('input[type=number]');
+// When size is submitted by the user, call makeGrid()
+document.querySelector('form').addEventListener('submit', makeGrid);
